2e13051247550016f1d4b086bdc80ff1174a33ce
Bart de Boer <bart.deboer@gmail.com>
2024-04-03 15:02:57 +0200

    initial commit

---
+++ uniqueid.go
@@ -0,0 +1,132 @@
+package uniqueid
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	lastTimestamp uint64
+	incr          uint64
+	mutex         sync.Mutex
+)
+
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+func pow(base, exponent uint64) uint64 {
+	result := uint64(1)
+	for exponent != 0 {
+		if exponent%2 == 1 {
+			result *= base
+		}
+		base *= base
+		exponent /= 2
+	}
+	return result
+}
+
+func reverseBytes(b []byte) []byte {
+	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return b
+}
+
+func generateUniqueID() string {
+	mutex.Lock()
+	defer mutex.Unlock()
+	currentTimestamp := uint64(time.Now().UnixNano())
+	if currentTimestamp == lastTimestamp {
+		incr++
+	} else {
+		lastTimestamp = currentTimestamp
+		incr = 0
+	}
+	randomPart := uint64(rand.Intn(1000))
+	combined := (lastTimestamp * 100000) + (incr * 1000) + randomPart
+	return EncodeBase62(combined)
+}
+
+func Encode(number uint64, base int) string {
+	if number == 0 {
+		return string(chars[0]) // Return the first character if number is 0
+	}
+	var encoded bytes.Buffer
+	uint64Base := uint64(base)
+	for number > 0 {
+		remainder := number % uint64Base
+		number /= uint64Base
+		encoded.WriteByte(chars[remainder])
+	}
+	return string(reverseBytes(encoded.Bytes()))
+}
+
+// func encode2(number uint64, base int) string {
+// 	if number == 0 {
+// 		return string(chars[0]) // Return the first character if number is 0
+// 	}
+// 	var encoded []byte
+// 	uint64Base := uint64(base)
+// 	for number > 0 {
+// 		remainder := number % uint64Base
+// 		number /= uint64Base
+// 		encoded = append([]byte{chars[remainder]}, encoded...)
+// 	}
+// 	return string(encoded)
+// }
+
+// func encode3(number uint64, base int) string {
+// 	var encoded string
+// 	uint64Base := uint64(base)
+// 	for number > 0 {
+// 		remainder := number % uint64Base
+// 		number /= uint64Base
+// 		encoded = string(chars[remainder]) + encoded
+// 	}
+// 	return encoded
+// }
+
+func Decode(encoded string, base int) (uint64, error) {
+	var number uint64 = 0
+	uint64Base := uint64(base)
+	for i, char := range encoded {
+		index := strings.IndexRune(chars, char)
+		if index == -1 {
+			return 0, fmt.Errorf("invalid character: %s\n", string(char))
+		}
+		number += uint64(index) * pow(uint64Base, uint64(len(encoded)-i-1))
+	}
+	return number, nil
+}
+
+func EncodeBase62(number uint64) string {
+	return Encode(number, 62)
+}
+
+func DecodeBase62(coded string) (uint64, error) {
+	return Decode(coded, 62)
+}
+
+func GenerateUnixTimestampID() string {
+	return EncodeBase62(uint64(time.Now().Unix()))
+}
+
+func GenerateUnixMiliTimestampID() string {
+	return EncodeBase62(uint64(time.Now().UnixMilli()))
+}
+
+func GenerateUnixMicroTimestampID() string {
+	return EncodeBase62(uint64(time.Now().UnixMicro()))
+}
+
+func GenerateUnixNanoTimestampID() string {
+	return EncodeBase62(uint64(time.Now().UnixNano()))
+}
+
+func Generate() string {
+	return generateUniqueID()
+}
---
+++ uniqueid_test.go
@@ -0,0 +1,56 @@
+package uniqueid
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+func TestEncodeDecode(t *testing.T) {
+	iterations := 50
+	for i := 0; i < iterations; i++ {
+		randomPart := uint64(rand.Intn(10000000000))
+		encoded := Encode(randomPart, 62)
+		decoded, _ := Decode(encoded, 62)
+		fmt.Printf("%d: %s -> %d\n", randomPart, encoded, decoded)
+		if randomPart != decoded {
+			t.Fatalf("Decoded int doesn't match original int %d: %s -> %d", randomPart, encoded, decoded)
+		}
+	}
+}
+
+// TestGenerate ensures that the Generate function returns a unique ID each time it's called
+func TestGenerate(t *testing.T) {
+	idMap := make(map[string]bool)
+	iterations := 1000000
+	for i := 0; i < iterations; i++ {
+		id := Generate()
+		if _, exists := idMap[id]; exists {
+			t.Fatalf("ID is not unique: %s", id)
+		}
+		idMap[id] = true
+	}
+}
+
+// TestGenerateFormat checks if the generated IDs are in the expected base62 format
+func TestGenerateFormat(t *testing.T) {
+	id := Generate()
+
+	fmt.Printf("Unix:  %s\n", GenerateUnixTimestampID())
+	fmt.Printf("Mili:  %s\n", GenerateUnixMiliTimestampID())
+	fmt.Printf("Micro: %s\n", GenerateUnixMicroTimestampID())
+	fmt.Printf("Nano:  %s\n", GenerateUnixNanoTimestampID())
+	fmt.Printf("ID:    %s\n", id)
+
+	// Very basic format check: length and allowed characters
+	// This might need to be adjusted based on the specifics of your ID format
+	if len(id) == 0 {
+		t.Fatalf("Generated ID is empty")
+	}
+
+	for _, char := range id {
+		if !((char >= '0' && char <= '9') || (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')) {
+			t.Fatalf("Generated ID contains unexpected characters: %s", id)
+		}
+	}
+}
